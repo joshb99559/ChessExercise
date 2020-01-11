c762dbd4c49e9c61bdcc3c1ad30ad6ab81fe0531
joshb99559 <joshb99559@gmail.com>
2020-01-11 16:22:25 -0500

    Added Gradle Support

---
+++ src/main/java/ChessExercise/Piece.java
@@ -0,0 +1,15 @@
+enum Piece {
+    WROOK,
+    WKNIGHT,
+    WBISHOP,
+    WQUEEN, 
+    WKING,
+    WPAWN,
+    BROOK,
+    BKNIGHT,
+    BBISHOP,
+    BQUEEN, 
+    BKING,
+    BPAWN,
+    EMPTY
+}
